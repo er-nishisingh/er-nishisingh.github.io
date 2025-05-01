850b5ee9bee300b3a1d108fc385af8c03b9fd4ae
er-nishisingh <105238986+er-nishisingh@users.noreply.github.com>
2025-05-01 10:34:40 +0530

    updating index.js

--- index.js
+++ index.js
@@ -92,3 +92,19 @@ $(document).ready(function () {
 
   $("footer .curr_yr").text(" " + new Date().getFullYear());
 });
+
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, "I") ||
+    ctrlShiftKey(e, "J") ||
+    ctrlShiftKey(e, "C") ||
+    (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+  )
+    return false;
+};
